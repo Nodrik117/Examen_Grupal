b7639e544e73c21e77d8f7972275d4cb15eea9ab
Nodrik117 <diegodamiandelacruz@gmail.com>
2024-11-28 00:30:10 -0500

    Menu

---
+++ database/migrations/2024_11_28_042303_create_antecedentes_personales.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('antecedentes_personales', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('alergia_antibiotico');
+            $table->boolean('alergia_anesstesia');
+            $table->boolean('asma');
+            $table->boolean('diabetes');
+            $table->boolean('hipertension');
+            $table->boolean('enfermedad_cardiaca');
+            $table->string('otros_antecedentes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('antecedentes_personales');
+    }
+};
---
+++ resources/views/menu.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="accordion max-w-5" id="accordionExample">
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingOne">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne"
+                    aria-expanded="true" aria-controls="collapseOne">
+                    Damian De La Cruz
+                </button>
+            </h2>
+            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
+                data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <div class="btn-group">
+                        <a href="{{url('/')}}" class="btn btn-primary">Crud 1</a>
+                        <a href="{{url('/')}}" class="btn btn-primary">Crud 2</a>
+                      </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingTwo">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    Juan Varela
+                </button>
+            </h2>
+            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+                data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <div class="btn-group">
+                        <a href="{{url('/')}}" class="btn btn-primary">Link</a>
+                        <a href="{{url('/')}}" class="btn btn-primary">Link</a>
+                      </div>
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingThree">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    Kathy Balarezo
+                </button>
+            </h2>
+            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
+                data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <div class="btn-group">
+                        <a href="{{url('/')}}" class="btn btn-primary">Link</a>
+                        <a href="{{url('/')}}" class="btn btn-primary">Link</a>
+                      </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingFour">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                    Anthony Venegas
+                </button>
+            </h2>
+            <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour"
+                data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <div class="btn-group">
+                        <a href="{{url('/')}}" class="btn btn-primary">Link</a>
+                        <a href="{{url('/')}}" class="btn btn-primary">Link</a>
+                      </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
